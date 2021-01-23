c7290ff001a077ee01d9c210dabd4127da280e21
Alexandru Polevoi <alexandru.polevoi.work@gmail.com>
2021-01-23 13:04:33 +0100

    refactor

--- PhotoSi.Orders.Server/Orders/Validation/Validator.cs
+++ PhotoSi.Orders.Server/Orders/Validation/Validator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using PhotoSi.Orders.Server.Orders.Controllers;
@@ -37,20 +38,7 @@ namespace PhotoSi.Orders.Server.Orders.Validation
 			if (order.Products.SelectMany(x => x.CustomOptions).Any(x => x.Id == Guid.Empty))
 				validationResult.AddErrorMessage<Guid>($"{nameof(OrderModel)}.{nameof(OrderModel.Products)}.{nameof(ProductModel.CustomOptions)}.{nameof(Option.Id)} property is required");
 
-			var productsWithDuplicatedOptions = order
-				.Products
-				.Where(product =>
-				{
-					var duplicatedOptions = product
-						.CustomOptions
-						.Select(x => x.Id)
-						.GroupBy(o => o)
-						.Count(o => o.ToList().Count > 1);
-
-					return duplicatedOptions > 0;
-				})
-				.Select(x => x.Id)
-				.ToList();
+			var productsWithDuplicatedOptions = GetProductsWithDuplicatedOptions(order.Products);
 			
 			if (productsWithDuplicatedOptions.Any())
 				validationResult.AddErrorMessage<Guid>($"{nameof(OrderModel)}.{nameof(OrderModel.Products)}.{nameof(ProductModel.CustomOptions)} products [{productsWithDuplicatedOptions.JoinStrings()}] contains duplicated options");
@@ -100,12 +88,32 @@ namespace PhotoSi.Orders.Server.Orders.Validation
 
 			order.Products
 				.ToList()
-				.ForEach(product => ValidateOptions(storedProducts, product, validationResult));
+				.Select(product => GetProductsWithMissingOptions(storedProducts, product))
+				.Where(x => x.Item2.Any())
+				.ToList()
+				.ForEach(x => validationResult.AddErrorMessage<Guid>($"{nameof(OrderModel)}.{nameof(OrderModel.Products)}.{nameof(ProductModel.CustomOptions)} product [{x.Item1}] has associated [{x.Item2.JoinStrings()}] options that does not exists"));
 			
 			return validationResult;
 		}
 
-		private static void ValidateOptions(RequestResult<Product, Guid> storedProducts, ProductModel product, ValidationResult validationResult)
+		private static List<Guid> GetProductsWithDuplicatedOptions(IEnumerable<ProductModel> products)
+		{
+			return products
+				.Where(product =>
+				{
+					var duplicatedOptions = product
+						.CustomOptions
+						.Select(x => x.Id)
+						.GroupBy(o => o)
+						.Count(o => o.ToList().Count > 1);
+
+					return duplicatedOptions > 0;
+				})
+				.Select(x => x.Id)
+				.ToList();
+		}
+
+		private static (Guid, List<Guid>) GetProductsWithMissingOptions(RequestResult<Product, Guid> storedProducts, ProductModel product)
 		{
 			var storedProduct = storedProducts
 				.GetList()
@@ -116,9 +124,8 @@ namespace PhotoSi.Orders.Server.Orders.Validation
 				.Select(x => x.Id)
 				.Except(storedProduct.Options.Select(x => x.Id))
 				.ToList();
-
-			if (notExistingOptions.Any())
-				validationResult.AddErrorMessage<Guid>($"{nameof(OrderModel)}.{nameof(OrderModel.Products)}.{nameof(ProductModel.CustomOptions)} product [{product.Id}] has associated [{notExistingOptions.JoinStrings()}] options that does not exists");
+			
+			return (product.Id, notExistingOptions);
 		}
 	}
 }
