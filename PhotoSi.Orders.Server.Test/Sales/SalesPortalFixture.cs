2a7a240fd4635bacf4f38c53d19f6828263c2a68
Alexandru Polevoi <alexandru.polevoi.work@gmail.com>
2021-01-23 13:22:34 +0100

    refactor

--- PhotoSi.Orders.Server.Test/Sales/SalesPortalFixture.cs
+++ PhotoSi.Orders.Server.Test/Sales/SalesPortalFixture.cs
@@ -14,16 +14,16 @@ namespace PhotoSi.Orders.Server.Test.Sales
 		public async Task ProcessOrder()
 		{
 			var order = new Order();
-			var persistence = new Mock<ISalesRepository>(MockBehavior.Strict);
-			persistence
+			var repository = new Mock<ISalesRepository>(MockBehavior.Strict);
+			repository
 				.Setup(x => x.SaveAsync(order))
 				.Returns(Task.CompletedTask)
 				.Verifiable("Save operation was not been performed");
-			var ordersEngine = new SalesPortal(persistence.Object);
+			var ordersEngine = new SalesPortal(repository.Object);
 
 			await ordersEngine.ProcessAsync(order);
 			
-			persistence.Verify();
+			repository.Verify();
 		}
 		
 		[Test]
@@ -31,19 +31,19 @@ namespace PhotoSi.Orders.Server.Test.Sales
 		{
 			var orderId = new Guid("2B5174E4-37B7-44EE-A8A2-EE920C6FAB9C");
 			var order = new Order();
-			var persistence = new Mock<ISalesRepository>(MockBehavior.Strict);
-			persistence
+			var repository = new Mock<ISalesRepository>(MockBehavior.Strict);
+			repository
 				.Setup(x => x.GetOrderAsync(orderId))
 				.ReturnsAsync(RequestResult<Order, Guid>.New(new [] { order }, new [] { orderId }))
 				.Verifiable("Get operation was not been performed");
-			var ordersEngine = new SalesPortal(persistence.Object);
+			var ordersEngine = new SalesPortal(repository.Object);
 
 			var requestResult = await ordersEngine.GetAsync(orderId);
 
 			Assert.That(requestResult, Is.Not.Null);
 			Assert.That(requestResult.FoundAll(), Is.True);
 			Assert.That(requestResult.GetScalar(), Is.EqualTo(order));
-			persistence.Verify();
+			repository.Verify();
 		}
 
 		[Test]
@@ -59,52 +59,52 @@ namespace PhotoSi.Orders.Server.Test.Sales
 				new Product()
 			};
 			var expectedRequestResult = RequestResult<Product, Guid>.New(products, productsIds);
-			var persistence = new Mock<ISalesRepository>(MockBehavior.Strict);
-			persistence
+			var repository = new Mock<ISalesRepository>(MockBehavior.Strict);
+			repository
 				.Setup(x => x.GetProductsAsync(productsIds))
 				.ReturnsAsync(expectedRequestResult)
 				.Verifiable("Get operation was not been performed");
-			var ordersEngine = new SalesPortal(persistence.Object);
+			var ordersEngine = new SalesPortal(repository.Object);
 
 			var actualRequestResult = await ordersEngine.GetProductsAsync(productsIds);
 
 			Assert.That(actualRequestResult, Is.Not.Null);
 			Assert.That(actualRequestResult, Is.EqualTo(expectedRequestResult));
-			persistence.Verify();
+			repository.Verify();
 		}
 
 		[Test]
 		public async Task ExistsCategory()
 		{
 			var categoryId = new Guid("2B5174E4-37B7-44EE-A8A2-EE920C6FAB9C");
-			var persistence = new Mock<ISalesRepository>(MockBehavior.Strict);
-			persistence
+			var repository = new Mock<ISalesRepository>(MockBehavior.Strict);
+			repository
 				.Setup(x => x.ExistsCategoryAsync(categoryId))
 				.ReturnsAsync(true)
 				.Verifiable("Exists operation was not been performed");
-			var ordersEngine = new SalesPortal(persistence.Object);
+			var ordersEngine = new SalesPortal(repository.Object);
 
 			var exists = await ordersEngine.ExistsCategoryAsync(categoryId);
 
 			Assert.That(exists, Is.True);
-			persistence.Verify();
+			repository.Verify();
 		}
 
 		[Test]
 		public async Task ExistsOrder()
 		{
 			var orderId = new Guid("2B5174E4-37B7-44EE-A8A2-EE920C6FAB9C");
-			var persistence = new Mock<ISalesRepository>(MockBehavior.Strict);
-			persistence
+			var repository = new Mock<ISalesRepository>(MockBehavior.Strict);
+			repository
 				.Setup(x => x.ExistsOrderAsync(orderId))
 				.ReturnsAsync(true)
 				.Verifiable("Exists operation was not been performed");
-			var ordersEngine = new SalesPortal(persistence.Object);
+			var ordersEngine = new SalesPortal(repository.Object);
 
 			var exists = await ordersEngine.ExistsOrderAsync(orderId);
 
 			Assert.That(exists, Is.True);
-			persistence.Verify();
+			repository.Verify();
 		}
 
 		[Test]
@@ -114,16 +114,16 @@ namespace PhotoSi.Orders.Server.Test.Sales
 			{
 				new Category()
 			};
-			var persistence = new Mock<ISalesRepository>(MockBehavior.Strict);
-			persistence
+			var repository = new Mock<ISalesRepository>(MockBehavior.Strict);
+			repository
 				.Setup(x => x.Upsert(categories))
 				.Returns(Task.CompletedTask)
 				.Verifiable("Upsert operation was not been performed");
-			var ordersEngine = new SalesPortal(persistence.Object);
+			var ordersEngine = new SalesPortal(repository.Object);
 
 			await ordersEngine.UpsertAsync(categories);
 
-			persistence.Verify();
+			repository.Verify();
 		}
 
 		[Test]
@@ -133,16 +133,16 @@ namespace PhotoSi.Orders.Server.Test.Sales
 			{
 				new Product()
 			};
-			var persistence = new Mock<ISalesRepository>(MockBehavior.Strict);
-			persistence
+			var repository = new Mock<ISalesRepository>(MockBehavior.Strict);
+			repository
 				.Setup(x => x.Upsert(products))
 				.Returns(Task.CompletedTask)
 				.Verifiable("Upsert operation was not been performed");
-			var ordersEngine = new SalesPortal(persistence.Object);
+			var ordersEngine = new SalesPortal(repository.Object);
 
 			await ordersEngine.UpsertAsync(products);
 
-			persistence.Verify();
+			repository.Verify();
 		}
 	}
 }
